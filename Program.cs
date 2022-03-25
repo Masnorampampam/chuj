b5b28d3c61194bc879e4c6dd686d3ea0455e5c43
Masnorampampam <101402924+Masnorampampam@users.noreply.github.com>
2022-03-25 13:28:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SzyfrCezara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O ile przesunać?");
+            int cezar;
+            cezar = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Jaki Teskt zamienić?");
+            foreach (var c in Console.ReadLine)
+            {
+                Console.WriteLine(Convert.ToChar((c - 'a' + cezar) % ('z' - 'a' + cezar) + 'a'));
+            }
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
